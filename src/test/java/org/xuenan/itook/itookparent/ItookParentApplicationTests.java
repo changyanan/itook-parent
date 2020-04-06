c5e72f4c159e09d31144c72e1125217ee92328d6
changyanan <82404677@QQ.COM>
2020-04-06 10:59:02 +0800

    first

---
+++ src/test/java/org/xuenan/itook/itookparent/ItookParentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xuenan.itook.itookparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItookParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
